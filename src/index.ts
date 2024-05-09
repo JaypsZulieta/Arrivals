77338168cd91a2660003c30be570a4e2463a72f0
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-10 06:04:03 +0800

    feat (routes): Add POST verb for  Guards Route

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Hammock } from "hammockjs";
 import { guardRoot } from "./routes/guardsRoot.route";
 import { authenticationRoute } from "./routes/authentication.route";
 import { guardRoute } from "./routes/guard.route";
+import { guardsRoute } from "./routes/guards.route";
 
 const hammock = new Hammock(8080);
 
@@ -13,5 +14,7 @@ hammock.addPUTRoute(authenticationRoute);
 hammock.addPATCHRoute(guardRoute);
 hammock.addPOSTRoute(guardRoot);
 hammock.addGETRoute(guardRoute);
+
+hammock.addPOSTRoute(guardsRoute);
 
 hammock.rest();
